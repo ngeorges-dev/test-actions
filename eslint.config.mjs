0f690f678356eef99a8889dd2a2b2c0683b2af42
ngeorges <ngeorges.dev@gmail.com>
2025-02-02 22:59:13 +0100

    feat: enhance ESLint configuration to include Jest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Les globales pour le navigateur
+        ...globals.jest, // Ajout des globales pour Jest
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
